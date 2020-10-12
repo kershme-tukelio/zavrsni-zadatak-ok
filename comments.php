2e3a6f043630c142528ab07850789ada88b69604
Jovan Zoric <jovan@example.com>
2020-10-12 20:39:56 +0200

    Drugi Zadatak izvrsen

---
+++ comments.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Vivify Blog</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <link href="styles/blog.css" rel="stylesheet">
+</head>
+<body class="va-l-page va-l-page--homepage">
+
+        <?php include('header.php'); ?>
+
+    <main role="main" class="container">
+
+        <?php include('sidebar.php'); ?>
+
+    </main>
+
+        <?php include('footer.php'); ?>
+
+</body>
+</html>
